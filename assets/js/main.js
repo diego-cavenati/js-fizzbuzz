4ed71d9742375763bea094f062a0a32300ce49f4
diego-cavenati <diegocavenati@gmail.com>
2022-10-10 15:17:57 +0200

    Bonus 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,28 +34,33 @@ for (let i = 1; i <= 100; i++) {
     // dichiaro div e gli aggiungo la sclasse square
     let div = document.createElement("div");
     div.classList.add("square");
+    let divChild = document.querySelector(".square");
 
     // prendo rowElement e gli aggiungo o il numero o fizz o buzz o fizzbuzz all'interno di un DIV con classe univoca square + classe personale nel caso di fixx, buzz e fizzbuzz
     
     if (i % 3 == 0 && i % 5 == 0) { // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
-        div.append("FizzBuzz");
-        div.classList.add("fizzbuzz");
+        divChild.append("FizzBuzz");
+        divChild.classList.add("fizzbuzz");
+        div.append(divChild);
         rowElement.append(div);
 
     } else if (i % 3 == 0) { // Per i multipli di 3 stampi “Fizz” al posto del numero
-        div.append("Fizz");
-        div.classList.add("fizz");
+        divChild.append("Fizz");
+        divChild.classList.add("fizz");
+        div.append(divChild);
         rowElement.append(div);
 
     } else if (i % 5 == 0) { // Per i multipli di 5 stampi “Buzz”
 
-        div.append("Buzz");
-        div.classList.add("buzz");
+        divChild.append("Buzz");
+        divChild.classList.add("buzz");
+        div.append(divChild);
         rowElement.append(div);
 
     } else {
 
-        div.append(i);
+        divChild.append(`${i}`);
+        div.append(divChild);
         rowElement.append(div);
 
     }   
